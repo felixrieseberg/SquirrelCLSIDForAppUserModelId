ef0962c9e1c055ccbf5edefb861a564459d7cf2e
Felix Rieseberg <felix@felixrieseberg.com>
2016-11-22 12:49:50 -0800

    :package: Add the codez

---
+++ SquirrelCLSIDForAppUserModelId/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquirrelCLSIDForAppUserModelId
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("ToastActivatorCLSID: " + CreateGuidFromHash(args[0]));
+                return;
+            }
+
+            Console.WriteLine("Create GUID for string:");
+            Console.WriteLine("ToastActivatorCLSID: " + CreateGuidFromHash(Console.ReadLine()));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+        public static readonly Guid IsoOidNamespace = new Guid("6ba7b812-9dad-11d1-80b4-00c04fd430c8");
+
+        public static Guid CreateGuidFromHash(string text)
+        {
+            return CreateGuidFromHash(Encoding.UTF8.GetBytes(text), Program.IsoOidNamespace);
+        }
+
+        public static Guid CreateGuidFromHash(byte[] nameBytes, Guid namespaceId)
+        {
+            // convert the namespace UUID to network order (step 3)
+            byte[] namespaceBytes = namespaceId.ToByteArray();
+            SwapByteOrder(namespaceBytes);
+
+            // comput the hash of the name space ID concatenated with the 
+            // name (step 4)
+            byte[] hash;
+            using (var algorithm = SHA1.Create())
+            {
+                algorithm.TransformBlock(namespaceBytes, 0, namespaceBytes.Length, null, 0);
+                algorithm.TransformFinalBlock(nameBytes, 0, nameBytes.Length);
+                hash = algorithm.Hash;
+            }
+
+            // most bytes from the hash are copied straight to the bytes of 
+            // the new GUID (steps 5-7, 9, 11-12)
+            var newGuid = new byte[16];
+            Array.Copy(hash, 0, newGuid, 0, 16);
+
+            // set the four most significant bits (bits 12 through 15) of 
+            // the time_hi_and_version field to the appropriate 4-bit 
+            // version number from Section 4.1.3 (step 8)
+            newGuid[6] = (byte)((newGuid[6] & 0x0F) | (5 << 4));
+
+            // set the two most significant bits (bits 6 and 7) of the 
+            // clock_seq_hi_and_reserved to zero and one, respectively 
+            // (step 10)
+            newGuid[8] = (byte)((newGuid[8] & 0x3F) | 0x80);
+
+            // convert the resulting UUID to local byte order (step 13)
+            SwapByteOrder(newGuid);
+            return new Guid(newGuid);
+        }
+
+        static void SwapByteOrder(byte[] guid)
+        {
+            SwapBytes(guid, 0, 3);
+            SwapBytes(guid, 1, 2);
+            SwapBytes(guid, 4, 5);
+            SwapBytes(guid, 6, 7);
+        }
+
+        static void SwapBytes(byte[] guid, int left, int right)
+        {
+            byte temp = guid[left];
+            guid[left] = guid[right];
+            guid[right] = temp;
+        }
+    }
+}
